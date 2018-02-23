35c2dc3feedbb96bcf5aa6c558b9d69bc49b9773
TugceBaydar <tugcebaydar06@gmail.com>
2018-02-22 21:01:26 -0800

    heyy

--- js/main.js
+++ js/main.js
@@ -11,22 +11,45 @@
 //   }
 // })
 
-new Vue ({
-   el: '#app',
-   data: {
-     progress: 0,
-   },
-   methods: {
-     start: function() {
-        var vm = this;
-        var width = 5;
-        var increase = setInterval(function() {
-          vm.progress += width;
-          if(vm.progress >= 100) {
-            clearInterval(increase);
-          }
-      }, 100);
+// new Vue ({
+//    el: '#app',
+//    data: {
+//      progress: 0,
+//    },
+//    methods: {
+//      start: function() {
+//         var vm = this;
+//         var width = 5;
+//         var increase = setInterval(function() {
+//           vm.progress += width;
+//           if(vm.progress >= 100) {
+//             clearInterval(increase);
+//           }
+//       }, 100);
+//
+//      }
+//    }
+// })
 
-     }
-   }
-})
+
+var todo = new Vue ({
+  el:'#todo',
+  data: {
+    newTask:'',
+    todoList:[]
+  },
+  methods: {
+    addTask: function() {
+      var task = this.newTask;
+      this.todoList.push(task);
+      this.newTask = '';
+    },
+    removeTask: function(task) {
+       var index = this.todoList.indexOf(task);
+       this.todoList.splice(index, 1);
+    }
+  }
+});
+
+
+// add animation and add editing thing !!
